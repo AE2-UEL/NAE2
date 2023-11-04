f8258e9df9937e05ba53515046348eb565fb7563
NotMyWing <winwyv@gmail.com>
2023-11-04 21:11:13 +1100

    Cull out PAE2 tooltips if CV is enabled

--- src/main/java/co/neeve/nae2/mixin/jei/cellview/MixinApiClientHelper.java
+++ src/main/java/co/neeve/nae2/mixin/jei/cellview/MixinApiClientHelper.java
@@ -1,8 +1,13 @@
 package co.neeve.nae2.mixin.jei.cellview;
 
+import appeng.api.storage.ICellInventory;
 import appeng.api.storage.ICellInventoryHandler;
 import appeng.api.storage.data.IAEStack;
+import appeng.api.storage.data.IItemList;
 import appeng.core.api.ApiClientHelper;
+import co.neeve.nae2.common.features.subfeatures.JEIFeatures;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import mezz.jei.config.KeyBindings;
 import net.minecraft.client.resources.I18n;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,4 +27,18 @@ public class MixinApiClientHelper {
 		lines.add("");
 		lines.add(I18n.format("nae2.jei.cellview.keybind", KeyBindings.showRecipe.getDisplayName()));
 	}
+
+	@WrapOperation(method = "addCellInformation", at = @At(
+		value = "INVOKE",
+		target = "Lappeng/api/storage/ICellInventory;getAvailableItems(Lappeng/api/storage/data/IItemList;)" +
+			"Lappeng/api/storage/data/IItemList;"
+	))
+	private <T extends IAEStack<T>> IItemList<T> getAvailableItems(ICellInventory<T> instance, IItemList<T> in,
+	                                                               Operation<IItemList<T>> operation) {
+		// Return the same list without filling it.
+		if (JEIFeatures.CELL_VIEW.isEnabled()) return in;
+
+		return operation.call(instance, in);
+	}
+
 }
