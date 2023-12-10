ddfe2a4c8c2a47e27e6b34e92d6c5fcbb7526a64
NotMyWing <winwyv@gmail.com>
2023-12-10 13:49:59 +1100

    Fix invalid Mixin target for upgrade slots

--- src/main/java/co/neeve/nae2/mixin/upgrades/base/MixinSlotRestrictedInput.java
+++ src/main/java/co/neeve/nae2/mixin/upgrades/base/MixinSlotRestrictedInput.java
@@ -10,15 +10,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = SlotRestrictedInput.class, remap = false)
+@Mixin(value = SlotRestrictedInput.class)
 public class MixinSlotRestrictedInput {
-	@Shadow
 	@Final
+	@Shadow(remap = false)
 	private SlotRestrictedInput.PlacableItemType which;
 
 	@Inject(method = "isItemValid", at = @At(
 		value = "INVOKE",
-		target = "Lappeng/api/definitions/IDefinitions;items()Lappeng/api/definitions/IItems;"
+		target = "Lappeng/api/definitions/IDefinitions;items()Lappeng/api/definitions/IItems;",
+		remap = false
 	), cancellable = true)
 	private void injectValidityCheck(ItemStack is, CallbackInfoReturnable<Boolean> cir) {
 		if (this.which == SlotRestrictedInput.PlacableItemType.UPGRADES) {
