22ff03b497b98db2df26b7123d6a979cc8b63da8
NotMyWing <winwyv@gmail.com>
2024-05-05 17:51:58 +1100

    Adjust Virtual Pattern completion logic... again

--- src/main/java/co/neeve/nae2/mixin/jei/craft/shared/MixinCraftingCPUCluster.java
+++ src/main/java/co/neeve/nae2/mixin/jei/craft/shared/MixinCraftingCPUCluster.java
@@ -3,23 +3,32 @@ package co.neeve.nae2.mixin.jei.craft.shared;
 import appeng.api.networking.crafting.ICraftingPatternDetails;
 import appeng.api.networking.energy.IEnergyGrid;
 import appeng.api.storage.data.IAEItemStack;
+import appeng.api.storage.data.IItemList;
 import appeng.me.cache.CraftingGridCache;
 import appeng.me.cluster.implementations.CraftingCPUCluster;
 import co.neeve.nae2.common.helpers.VirtualPatternDetails;
 import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.util.Map;
+
 @Mixin(value = CraftingCPUCluster.class, remap = false)
 public abstract class MixinCraftingCPUCluster {
 	@Shadow
-	private long remainingItemCount;
+	private IItemList<IAEItemStack> waitingFor;
+
+	@SuppressWarnings("rawtypes")
+	@Shadow
+	@Final
+	private Map tasks;
 
 	@Shadow
-	private IAEItemStack finalOutput;
+	protected abstract void completeJob();
 
 	@Shadow
 	public abstract void cancel();
@@ -35,7 +44,8 @@ public abstract class MixinCraftingCPUCluster {
 	public void executeCrafting(IEnergyGrid eg, CraftingGridCache cc, CallbackInfo ci,
 	                            @Local ICraftingPatternDetails details) {
 		if (details instanceof VirtualPatternDetails) {
-			if (this.remainingItemCount == this.finalOutput.getStackSize()) {
+			if (this.tasks.size() <= 1 && this.waitingFor.isEmpty()) {
+				this.completeJob();
 				this.cancel();
 				ci.cancel();
 			}
