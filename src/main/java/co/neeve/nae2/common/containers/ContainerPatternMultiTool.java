5d6352ef6385180e12ac19c0f10e90a088bc031a
NotMyWing <winwyv@gmail.com>
2025-03-14 16:26:12 +1100

    Update dependencies and use latest FCR API

--- src/main/java/co/neeve/nae2/common/containers/ContainerPatternMultiTool.java
+++ src/main/java/co/neeve/nae2/common/containers/ContainerPatternMultiTool.java
@@ -29,6 +29,7 @@ import co.neeve.nae2.common.slots.SlotPatternMultiTool;
 import co.neeve.nae2.common.slots.SlotPatternMultiToolUpgrade;
 import com.glodblock.github.common.item.ItemFluidDrop;
 import com.glodblock.github.common.item.ItemFluidEncodedPattern;
+import com.glodblock.github.common.item.fake.FakeItemRegister;
 import com.google.common.collect.Lists;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.InventoryPlayer;
@@ -40,6 +41,7 @@ import net.minecraft.item.crafting.CraftingManager;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.relauncher.Side;
@@ -262,7 +264,7 @@ public class ContainerPatternMultiTool extends AEBaseContainer implements IAEApp
 				}
 			}
 
-			if (dropList.size() > 0) {
+			if (!dropList.isEmpty()) {
 				var tileEntity = this.iface.getTileEntity();
 				var world = tileEntity.getWorld();
 				var blockPos = tileEntity.getPos();
@@ -327,7 +329,7 @@ public class ContainerPatternMultiTool extends AEBaseContainer implements IAEApp
 								list.set(idx, data);
 							} else continue;
 						} else if (fluidReplacement && stack.getItem() instanceof ItemFluidDrop) {
-							var fluidStack = ItemFluidDrop.getFluidStack(stack);
+							FluidStack fluidStack = FakeItemRegister.getStack(stack);
 
 							// This should never be a crafting pattern.
 							if (fluidStackIn.isFluidEqual(fluidStack)) {
